5cfe11771dd39de54c2078a76f9b7a7e6cadfb97
laveeshsanadhya <laveeshsanadhya@gmail.com>
2025-04-09 15:31:46 +0530

    updated the color project : using useState

--- ColorSwitch/src/App.jsx
+++ ColorSwitch/src/App.jsx
@@ -1,32 +1,36 @@
+import { useState } from "react";
 import tailwindcss from "tailwindcss";
 
 function App() {
-  const changeBackground = function (color){
+  const setBackground = function (color){
     return function(){
-      document.getElementById('body').style.backgroundColor = color ; 
+     setbg(color) ; 
 
     }
   }
+// using state 
+
+let  [bg , setbg]  = useState("white") ; 
   return (
     <>
       <div className="flex flex-col h-screen">
         {/* Main content */}
-        <div className="flex-grow"></div>
+        <div className="flex-grow" style={{backgroundColor : bg}}></div>
 
         {/* Buttons at the bottom */}
-        <div className="absolute bottom-0 w-full flex justify-center gap-4 p-4 bg-black rounded-full">
+        <div className="absolute bottom-0 w-full flex justify-center gap-4 p-4 bg-black rounded-full" >
           <button className="px-4 py-2 bg-red-500 text-white rounded"
-          onClick={changeBackground("red")}>Red</button>
+          onClick= {setBackground("red")}>Red</button>
           <button className="px-4 py-2 bg-blue-500 text-white rounded"
-          onClick={changeBackground("blue")}>Blue</button>
+          onClick= {setBackground("blue")}>Blue</button>
           <button className="px-4 py-2 bg-green-500 text-white rounded"
-          onClick={changeBackground("Green")}>Green</button>
+          onClick= {setBackground("Green")}>Green</button>
           <button className="px-4 py-2 bg-yellow-500 text-white rounded"
-          onClick={changeBackground("yellow")}>Yellow</button>
+          onClick= {setBackground("yellow")}>Yellow</button>
           <button className="px-4 py-2 bg-purple-500 text-white rounded"
-          onClick={changeBackground("purple")}>Purple</button>
+          onClick= {setBackground("purple")}>Purple</button>
           <button className="px-4 py-2 bg-pink-500 text-white rounded"
-          onClick={changeBackground("pink")}>Pink</button>
+          onClick= {setBackground("pink")}>Pink</button>
         </div>
       </div>
     </>
